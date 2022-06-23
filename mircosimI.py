42276ca908062d2a3fdca9045e690516ea3d9540
jwannebo3524 <jwannebo3524@stu.d214.org>
2022-06-23 17:06:46 -0500

    ug did nothing
    begining

---
+++ mircosimI.py
@@ -0,0 +1,8 @@
+import numpy as np
+#youve got to have a biasis,
+#a way to evaluate it all
+class Distribution:
+    def __init__(self):
+        self.Variables = []
+        self.Formulae = []
+    
